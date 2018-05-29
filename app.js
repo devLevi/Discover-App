8ddc19e94d2210529031cbc2bd5fa6c812815d32
Levi Bernard <levichristianbernard@gmail.com>
2018-05-28 17:04:22 -0700

    successfully pulled data object from Taste Div API

--- app.js
+++ app.js
@@ -2,45 +2,49 @@ const TASTEDIVE_SEARCH_URL = 'https://tastedive.com/api/similar';
 const apiKey = '309203-mediasea-3T1SAXA6';
 
 function getDataFromApi(searchTerm, callback) {
-  const settings = {
-    url: TASTEDIVE_SEARCH_URL,
-    data: {
-        k: apiKey,
-        q: searchTerm,
-    },
-    dataType: 'json',
-    type: 'GET',
-    success: callback
-  };
+    const settings = {
+        url: TASTEDIVE_SEARCH_URL,
+        data: {
+            k: apiKey,
+            q: searchTerm,
+        },
+        dataType: 'json',
+        type: 'GET',
+        success: callback
+    };
 
-  $.ajax(settings);
+    $.ajax(settings);
 }
 
 function renderResult(result) {
     return `
     <div class="injected-results-container">
       <div>
-        <a class="js-result-name" href="https://tastedive.com/like${result.name}" target="_blank">${result.name}</a>
+        <a class="js-result-name" href="https://tastedive.com/like/${result.Name}" target="_blank">${result.Name}</a>
+        <div>${result.Type}</div>
       </div>
+      <br></br>
     </div>
   `;
 }
 
 function displayTasteDiveSearchData(data) {
-  const results = data.results.map((result) => renderResult(result));
-  $('.js-search-results').html(results);
+    console.log('Data object retrieved:');
+    console.log(data);
+    const results = data.Similar.Results.map((item) => renderResult(item));
+    $('.js-search-results').html(results);
 }
 
 function watchSubmit() {
-  $('.js-search-form').submit(event => {
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
-    // clear out the input
-    queryTarget.val("");
-     // $('main').prop('hidden', false);
-    getDataFromApi(query, displayTasteDiveSearchData);
-  });
+    $('.js-search-form').submit(event => {
+        event.preventDefault();
+        const queryTarget = $(event.currentTarget).find('.js-query');
+        const query = queryTarget.val();
+        // clear out the input
+        queryTarget.val("");
+        // $('main').prop('hidden', false);
+        getDataFromApi(query, displayTasteDiveSearchData);
+    });
 }
 
 $(watchSubmit);
