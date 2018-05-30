ecd8f8fc0743d6b1f71931c372341b340f4abfc6
Levi Bernard <levichristianbernard@gmail.com>
2018-05-29 19:21:55 -0700

    added functionality and next page

--- app.js
+++ app.js
@@ -1,3 +1,43 @@
+function generateSearchPageHTML() {
+    return `
+
+    <div class="search-form-container">
+        <form action="#" class="search-form js-search-form">
+            <label for="query">Input the name of any book, band or movie to find similar results!</label>
+            <input type="text" id="query" class="js-query">
+            <button type="submit" class="search-button">Discover</button>
+        </form>
+    </div>
+        <main role="main" aria-live="assertive" >
+            <div class="results-container">
+                <h2>Results</h2>
+                <div class="js-search-results"></div>
+            </div>
+        </main>
+        `
+}
+
+function renderView(viewHtml) {
+    $('body').html(viewHtml);
+}
+
+function renderSearchPageHTML() {
+    renderView(generateSearchPageHTML);
+}
+
+function generateResultsPageHTML() {
+    return `
+        <div class="results-container">
+            <h2>Results</h2>
+            <div class="js-search-results"></div>
+        </div>
+    `
+}
+
+function renderResultsPageHTML() {
+    renderView(generateResultsPageHTML);
+}
+
 const TASTEDIVE_SEARCH_URL = 'https://tastedive.com/api/similar';
 const apiKey = '309203-mediasea-3T1SAXA6';
 
@@ -29,22 +69,22 @@ function renderResult(result) {
 }
 
 function displayTasteDiveSearchData(data) {
-    console.log('Data object retrieved:');
-    console.log(data);
     const results = data.Similar.Results.map((item) => renderResult(item));
     $('.js-search-results').html(results);
 }
 
-function watchSubmit() {
-    $('.js-search-form').submit(event => {
-        event.preventDefault();
+function searchSubmit() {
         const queryTarget = $(event.currentTarget).find('.js-query');
         const query = queryTarget.val();
         // clear out the input
         queryTarget.val("");
-        // $('main').prop('hidden', false);
+        $('main').prop('hidden', false);
         getDataFromApi(query, displayTasteDiveSearchData);
-    });
 }
 
-$(watchSubmit);
+function registerEventHandlers() {
+    $('.form-container').on('click', '.enter-page-button', renderSearchPageHTML);
+    $('.search-form-container').on('sumit', '.search-button', searchSubmit);
+}
+
+$(registerEventHandlers);
