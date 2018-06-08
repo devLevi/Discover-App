692dd731af3c7f070dd418bee84ffeea0fe4a888
Levi Bernard <levibernard@Levis-MacBook-Pro.local>
2018-06-08 01:32:35 -0700

    setup event listeners, updated dropdown menue, restricted search with categories

--- app.js
+++ app.js
@@ -3,141 +3,163 @@ const WALMART_SEARCH_URL = 'http://api.walmartlabs.com/v1/search';
 const apiKeyTasteDive = '309203-mediasea-3T1SAXA6';
 const apiKeyWalmart = 'k4ja8d6q36qp2bpg54b5s7pg';
 
-function displaySearchInput() {
-  $('.enter-page').on('click', '.discover', function() {
-  $('.enter-page').hide();
-  $('#info-div').show();
-  $('.search-input-container').show();
-  $('.results-container').hide();
-  $('.js-walmart-search-results').hide();
-  $('.start-over-btn-container').hide();
-})}
+$(setupEventListeners);
 
-displaySearchInput();
+function setupEventListeners() {
+    showLandingPageView();
+    $('.enter-page').on('click', '.discover', showSearchInput);
+    $('.js-tastedive-search-results').on('click', '.buy-links', handlePurchaseLinksBtn);
+    $('.js-search-form').submit(handleSearchSubmitInput);
+}
 
-function displayLandingPage() {
-  $(document).ready(function() {
+function showLandingPageView() {
   $('.enter-page').show();
   $('#info-div').hide();
   $('.search-input-container').hide();
   $('.results-container').hide();
-  $('.js-walmart-search-results').hide();
+  $('.purchase-links-container').hide();
   $('.start-over-btn-container').hide();
-})}
+}
 
-displayLandingPage();
+function showSearchInput() {
+    $('.enter-page').hide();
+    $('#info-div').show();
+    $('.search-input-container').show();
+    $('.results-container').hide();
+    $('.purchase-links-container').hide();
+    $('.start-over-btn-container').hide();
+}
+
+function showSimilarResultsView() {
+    $('.enter-page').hide();
+    $('#info-div').hide();
+    $('.search-input-container').hide();
+    $('.results-container').show();
+    $('.purchase-links-container').hide();
+    $('.start-over-btn-container').hide();
+}
+
+function showPurchaseLinks() {
+    $('.enter-page').hide();
+    $('#info-div').hide();
+    $('.search-input-container').hide();
+    $('.results-container').hide();
+    $('.purchase-links-container').show();
+    $('.start-over-btn-container').show();
+}
 
 function getDataFromTasteDiveApi(searchTerm, categoryTerm, callback) {
-    const settings = {
-        url: TASTEDIVE_SEARCH_URL,
-        data: {
-            k: apiKeyTasteDive,
-            q: searchTerm,
-            type: categoryTerm
-        },
-        dataType: 'jsonp',
-        type: 'GET',
-        success: callback
-    };
-    $.ajax(settings);
+  const settings = {
+    url: TASTEDIVE_SEARCH_URL,
+    data: {
+      k: apiKeyTasteDive,
+      q: searchTerm,
+      type: categoryTerm
+    },
+    dataType: 'jsonp',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
 }
 
 function generateTasteDiveResult(result) {
-    return `
+  return `
     <div class="injected-results-container">
       <div>
         <div class="js-result-name">${result.Name}</div>
-        <div>${result.Type}</div>
-        <button type="submit" class="buy-links">Purchase this ${result.Type}</button>
+          <div>${result.Type}</div>
+          <button type="button" class="buy-links">Purchase this ${result.Type}</button>
       </div>
       <br></br>
     </div>
   `;
 }
 
-
-
 function displayTasteDiveSearchData(data) {
-    const results = data.Similar.Results.map((item) => generateTasteDiveResult(item));
-    $('.js-tastedive-search-results').html(results);
+    // if (data.Similar.results.length === 0) {
+    //     $('.js-tastedive-search-results').html(generateEmptyTastediveResults());
+    // } else {
+    //
+    // }
+  const results = data.Similar.Results.map((item) => generateTasteDiveResult(item));
+  $('.js-tastedive-search-results').html(results);
 }
 
-function getDataFromWalmartApi(searchTerm, callback) {
-    const settings = {
-        url: WALMART_SEARCH_URL,
-        data: {
-            apiKey: apiKeyWalmart,
-            query: searchTerm
-        },
-        dataType: 'jsonp',
-        type: 'GET',
-        success: callback
-    };
-    $.ajax(settings);
+function getDataFromWalmartApi(searchTerm, categoryCode, callback) {
+  const settings = {
+    url: WALMART_SEARCH_URL,
+    data: {
+      apiKey: apiKeyWalmart,
+      query: searchTerm,
+      categoryId: categoryCode
+    },
+    dataType: 'jsonp',
+    type: 'GET',
+    success: callback
+  };
+  $.ajax(settings);
 }
 
 function generateWalmartResult(result) {
-    return `
+    let customerRatingTemplate = ``;
+    if (result.customerRating) {
+        customerRatingTemplate = `
+        <img src="${result.customerRatingImage}">
+        `;
+    } else {
+    customerRatingTemplate = `
+    <div>no customer ratings</div>
+    `;
+    }
+    debugger;
+  return `
     <div class="injected-results-container">
       <div>
         <a class="js-result-name" href="${result.addToCartUrl}" target="_blank"><img src="${result.mediumImage}"</a>
         <div>${result.name}</div>
-        <br>
-        <div>Customer Rating: "${result.customerRating}" </div>
-        <img src="${result.customerRatingImage}">
+        `+ customerRatingTemplate +`
       </div>
     </div>
+    <br>
   `;
 }
 
+
+
 function displayWalmartSearchData(data) {
-    // this function should display the buy links for the selected taste dive result.
-    debugger;
-    const results = data.items.map((item) => generateWalmartResult(item));
-    $('.js-walmart-search-results').html(results);
+  const results = data.items.map((item) => generateWalmartResult(item));
+  $('.js-walmart-search-results').html(results);
+  // showPurchaseLinksView();
 }
 
 function handlePurchaseLinksBtn() {
-    $('.js-tastedive-search-results').on('click', '.buy-links', function () {
-        event.preventDefault();
-        // debugger;
-        const $queryTarget = $(event.target)
-          .closest('.injected-results-container')
-          .find('.js-result-name'); // use .closest before
-        const query = $queryTarget.text(); // use .text()
-        getDataFromWalmartApi(query, displayWalmartSearchData);
-    });
-    /* $('.buy-links').click(function(event) { // change to .click()
-        alert('Event handler working!')
-        event.preventDefault();
-        const $queryTarget = $(event.currentTarget)
-          .closest('.injected-results-container')
-          .find('.js-result-name'); // use .closest before
-        const query = $queryTarget.text(); // use .text()
-        alert(query);
-        getDataFromWalmartApi(query, displayWalmartSearchData);
-    }); */
+    event.preventDefault();
+    // debugger;
+    showPurchaseLinks();
+    const $queryTarget = $(event.target)
+      .closest('.injected-results-container')
+      .find('.js-result-name');
+    const query = $queryTarget.text();
+    let category = $('#menu-values').val();
+    debugger;
+    const CATEGORY_CODE = {
+        movies: 4096,
+        music: 4104,
+        books: 3920
+    }
+    getDataFromWalmartApi(query, CATEGORY_CODE[category], displayWalmartSearchData);
 }
 
-function handleSearchSubmitInput() {
-    $('.js-search-form').submit(function(event) {
-        event.preventDefault();
-        const queryTarget = $(event.currentTarget).find('.js-query');
-        const query = queryTarget.val();
-        // clear out the input
-        queryTarget.val("");
-        $('main').prop('hidden', false);
-        // get value from category Dropdown
-        // const category = $('...').text();
-        getDataFromTasteDiveApi(query, displayTasteDiveSearchData);
-
-    });
+function handleSearchSubmitInput(event) {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    $('main').prop('hidden', false);
+    const category = $('.dropdown-menu').find(':selected').text();
+    showSimilarResultsView();
+    // debugger;
+    getDataFromTasteDiveApi(query, category, displayTasteDiveSearchData);
 }
-
-function handleSelectedResult () {
-  $(handleSearchSubmitInput);
-  // handleSearchSubmitInput();
-  $(handlePurchaseLinksBtn);
-}
-
-$(handleSelectedResult);
