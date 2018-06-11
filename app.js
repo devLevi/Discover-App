60467fc309d24a8c155e22527f67cd388d5c360d
nadia bernard <nadiabernard@nadias-MacBook-Pro.local>
2018-06-11 01:28:05 -0700

    added hover state to buttons

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ const apiKeyWalmart = 'k4ja8d6q36qp2bpg54b5s7pg';
 $(setupEventListeners);
 
 function setupEventListeners() {
-    showLandingPageView();
-    $('.splash-screen-view').on('click', '.discover', showSearchInput);
-    $('.js-tastedive-search-results').on('click', '.buy-links-btn', handlePurchaseLinksBtn);
-    $('.js-search-form').submit(handleSearchSubmitInput);
-    $('.start-over-btn-container').on('click', '.start-over-btn', showSearchInput);
-    $('.back-to-similar-results-container').on('click', '.similar-results-btn', showSimilarResultsView)
+  showLandingPageView();
+  $('.splash-screen-view').on('click', '.discover', showSearchInput);
+  $('.js-tastedive-search-results').on('click', '.buy-links-btn', handlePurchaseLinksBtn);
+  $('.js-search-form').submit(handleSearchSubmitInput);
+  $('.start-over-btn-container').on('click', '.start-over-btn', showSearchInput);
+  $('.back-to-similar-results-container').on('click', '.similar-results-btn', showSimilarResultsView);
 }
 
 
@@ -25,30 +25,30 @@ function showLandingPageView() {
 }
 
 function showSearchInput() {
-    $('.splash-screen-view').hide();
-    $('.main-view').show();
-    $('.js-similar-results-view').hide();
-    $('.js-purchase-results-view').hide();
-    $('.start-over-btn-container').hide();
-    $('.back-to-similar-results-container').hide();
+  $('.splash-screen-view').hide();
+  $('.main-view').show();
+  $('.js-similar-results-view').hide();
+  $('.js-purchase-results-view').hide();
+  $('.start-over-btn-container').hide();
+  $('.back-to-similar-results-container').hide();
 }
 
 function showSimilarResultsView() {
-    $('.splash-screen-view').hide();
-    $('.main-view').hide();
-    $('.js-similar-results-view').show();
-    $('.js-purchase-results-view').hide();
-    $('.start-over-btn-container').show();
-    $('.back-to-similar-results-container').hide();
+  $('.splash-screen-view').hide();
+  $('.main-view').hide();
+  $('.js-similar-results-view').show();
+  $('.js-purchase-results-view').hide();
+  $('.start-over-btn-container').show();
+  $('.back-to-similar-results-container').hide();
 }
 
 function showPurchaseLinks() {
-    $('.splash-screen-view').hide();
-    $('.main-view').hide();
-    $('.js-similar-results-view').hide();
-    $('.js-purchase-results-view').show();
-    $('.start-over-btn-container').show();
-    $('.back-to-similar-results-container').show();
+  $('.splash-screen-view').hide();
+  $('.main-view').hide();
+  $('.js-similar-results-view').hide();
+  $('.js-purchase-results-view').show();
+  $('.start-over-btn-container').show();
+  $('.back-to-similar-results-container').show();
 }
 
 function getDataFromTasteDiveApi(searchTerm, categoryTerm, callback) {
@@ -69,28 +69,26 @@ function getDataFromTasteDiveApi(searchTerm, categoryTerm, callback) {
 function generateTasteDiveResult(result) {
   return `
     <div class="injected-js-similar-results">
-      <div>
         <div class="js-result-name">${result.Name}</div>
           <button type="button" class="buy-links-btn">Purchase this ${result.Type}</button>
-      </div>
       <br></br>
     </div>
   `;
 }
 
-function generateEmptyTasteDiveResults () {
+function generateEmptyTasteDiveResults() {
   return `
     <div class="no-results-view">Oops! Nothing was found.. Please try again!</div>
   `
 }
 
 function displayTasteDiveSearchData(data) {
-    if (data.Similar.Results.length === 0) {
-        $('.js-tastedive-search-results').html(generateEmptyTasteDiveResults());
-    } else {
-      const results = data.Similar.Results.map((item) => generateTasteDiveResult(item));
-      $('.js-tastedive-search-results').html(results);
-    }
+  if (data.Similar.Results.length === 0) {
+    $('.js-tastedive-search-results').html(generateEmptyTasteDiveResults());
+  } else {
+    const results = data.Similar.Results.map((item) => generateTasteDiveResult(item));
+    $('.js-tastedive-search-results').html(results);
+  }
 }
 
 function getDataFromWalmartApi(searchTerm, categoryCode, callback) {
@@ -109,12 +107,12 @@ function getDataFromWalmartApi(searchTerm, categoryCode, callback) {
 }
 
 function generateWalmartResult(result) {
-    let customerRatingTemplate = ``;
-    if (result.customerRating) {
-        customerRatingTemplate = `<img src="${result.customerRatingImage}">`;
-    } else {
-        customerRatingTemplate = `not reviewed`;
-    }
+  let customerRatingTemplate = ``;
+  if (result.customerRating) {
+    customerRatingTemplate = `<img src="${result.customerRatingImage}">`;
+  } else {
+    customerRatingTemplate = `not reviewed`;
+  }
   return `
     <div class="injected-js-purchase-results">
       <div>
@@ -134,28 +132,28 @@ function displayWalmartSearchData(data) {
 }
 
 function handlePurchaseLinksBtn() {
-    event.preventDefault();
-    showPurchaseLinks();
-    const $queryTarget = $(event.target)
-      .closest('.injected-js-similar-results')
-      .find('.js-result-name');
-    const query = $queryTarget.text();
-    let category = $('#menu-values').val();
-    const CATEGORY_CODE = {
-        movies: 4096,
-        music: 4104,
-        books: 3920
-    }
-    getDataFromWalmartApi(query, CATEGORY_CODE[category], displayWalmartSearchData);
+  event.preventDefault();
+  showPurchaseLinks();
+  const $queryTarget = $(event.target)
+    .closest('.injected-js-similar-results')
+    .find('.js-result-name');
+  const query = $queryTarget.text();
+  let category = $('#menu-values').val();
+  const CATEGORY_CODE = {
+    movies: 4096,
+    music: 4104,
+    books: 3920
+  }
+  getDataFromWalmartApi(query, CATEGORY_CODE[category], displayWalmartSearchData);
 }
 
 function handleSearchSubmitInput(event) {
-    event.preventDefault();
-    const queryTarget = $(event.currentTarget).find('.js-query');
-    const query = queryTarget.val();
-    queryTarget.val("");
-    $('main').prop('hidden', false);
-    const category = $('.dropdown-menu').find(':selected').text();
-    showSimilarResultsView();
-    getDataFromTasteDiveApi(query, category, displayTasteDiveSearchData);
+  event.preventDefault();
+  const queryTarget = $(event.currentTarget).find('.js-query');
+  const query = queryTarget.val();
+  queryTarget.val("");
+  $('main').prop('hidden', false);
+  const category = $('.dropdown-menu').find(':selected').text();
+  showSimilarResultsView();
+  getDataFromTasteDiveApi(query, category, displayTasteDiveSearchData);
 }
